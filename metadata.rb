7500df4b33fa211f48f0ea0fdc607ec7d78799b4
Jeremy Olliver <jeremy.olliver@gmail.com>
2013-07-16 17:11:48 +1200

    Bump minor version and declare additional support

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,12 @@ maintainer_email "jeremy.olliver@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures newrelic_sysmond"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.2.0"
 
-depends "apt", ">= 1.8.2" # Some earlier versions don't correctly trigger an apt-get update after adding the repo
+recommends "apt", ">= 1.8.2" # Some earlier versions don't correctly trigger an apt-get update after adding the repo
 
-supports "ubuntu"
-supports "debian"
+%w{ ubuntu debian redhat centos amazon scientific }.each do |os|
+  supports os
+end
+
+provides "service[newrelic-sysmond]"
